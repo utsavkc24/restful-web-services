326aafefcb6bd45511537e211a6f8625bd95f4e4
unknown <utsavkc24@gmail.com>
2021-12-18 20:22:31 +0530

    Implementing DELETE Method to delete a User Resourc

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ public class UserResource {
 	public List<User> retrieveAllUser() {
 		return service.findAll();
 	}
+	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
@@ -32,6 +34,7 @@ public class UserResource {
 		
 		return user;
 	}
+	
 	@PostMapping("/users")
 	public ResponseEntity<Object> addUser(@RequestBody User user) {
 		User savedUser = service.save(user);
@@ -42,5 +45,12 @@ public class UserResource {
 				.buildAndExpand(savedUser.getId()).toUri();
 		
 		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		if(user == null)
+			 throw new UserNotFoundException("id -" +id);
 	}
 }
