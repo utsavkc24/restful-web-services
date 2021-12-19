a1bebfebfdb95a58be80c4729df170eb7dc6a618
unknown <utsavkc24@gmail.com>
2021-12-19 20:27:37 +0530

    Internationalization for RESTful Services

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,8 +5,10 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,6 +30,9 @@ public class UserResource {
 
 	@Autowired
 	private UserDaoService service;
+	
+	@Autowired
+	private ResourceBundleMessageSource resourceBundleMessageSource;
 
 	@GetMapping("/users")
 	public List<User> retrieveAllUser() {
@@ -62,5 +68,9 @@ public class UserResource {
 		User user = service.deleteById(id);
 		if (user == null)
 			throw new UserNotFoundException("id -" + id);
+	}
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return resourceBundleMessageSource.getMessage("good.morning.message", null, locale);
 	}
 }
