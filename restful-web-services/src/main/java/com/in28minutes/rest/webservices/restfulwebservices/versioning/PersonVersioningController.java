207f22aa65f760a8af8273a0381670492cea3839
unknown <utsavkc24@gmail.com>
2021-12-23 21:33:09 +0530

    Basic spring security application

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,53 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+
+	@GetMapping("v1/person")
+	public PersonV1 personV1() {
+		return new PersonV1("Bruce Wayne");
+	}
+
+	@GetMapping("v2/person")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("Bruce", "Wayne"));
+	}
+
+	
+	
+	@GetMapping(value = "/person/para", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bruce Wayne");
+	}
+
+	@GetMapping(value = "person/params", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bruce", "Wayne"));
+	}
+	
+	
+
+	@GetMapping(value = "/person/headers", headers = "X-API-VERSION=1")
+	public PersonV1 headersV1() {
+		return new PersonV1("Bruce Wayne");
+	}
+
+	@GetMapping(value = "/person/headers", headers = "X-API-VERSION=2")
+	public PersonV2 headersV2() {
+		return new PersonV2(new Name("Bruce", "Wayne"));
+	}
+	
+	
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+}
