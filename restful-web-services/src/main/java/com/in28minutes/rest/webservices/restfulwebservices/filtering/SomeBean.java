75782b1358aa0c9ab8ae3ac5316eb6a578989ddc
unknown <utsavkc24@gmail.com>
2021-12-23 20:31:53 +0530

    Implementing Dynamic Filtering for RESTful Service

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,13 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties(value = {"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
 	private String field1;
 	private String field2;
-	//@JsonIgnore
 	private String field3;
 
 	public SomeBean(String field1, String field2, String field3) {
